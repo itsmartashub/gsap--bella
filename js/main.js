d6692a59b237e1d1034fa02700c6ba0106339222
itsmartashub <coderawii@gmail.com>
2022-02-05 20:23:37 +0100

    Module 2 - Reveal Gallery
    nisam zadovoljna na mobile kako izgleda, wtf

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 
+let sections = document.querySelectorAll('.rg__column');
+
 function initNavigation() {
 	const mainNavLinks = gsap.utils.toArray('.main-nav a'); //? [a, a, a, a, a]  ovo ce biti array od nasih linkova, da posle mozemo da lupujemo kroz njih
 	// gsap.utils.toArray() --> selector text (returns the raw elements wrapped in an Array)
@@ -47,6 +49,68 @@ function initHeaderTilt() {
 	document.querySelector('header').addEventListener('mousemove', moveImages);
 }
 
+function initHoverReveal() {
+	sections.forEach(section => {
+		// console.log(typeof section); //!!!! posto je ovo object, zato mozemo da mu attached neke propertije poput imageBlock i mask koji ce biti selektori, i posle ih pozovemo tj koristimo u onoj createHoverReaveal f-ji
+
+		// get components for animations
+		section.imageBlock = section.querySelector('.rg__image');
+		section.image = section.querySelector('.rg__image img');
+		section.mask = section.querySelector('.rg__image--mask');
+		section.text = section.querySelector('.rg__text');
+		section.textCopy = section.querySelector('.rg__text--copy');
+		section.textMask = section.querySelector('.rg__text--mask');
+		section.textP = section.querySelector('.rg__text--copy p');
+
+		// reset the initial position
+		gsap.set([section.imageBlock, section.textMask], { yPercent: -101 });
+		gsap.set([section.mask, section.textP], { yPercent: 100 });
+		gsap.set(section.image, { scale: 1.2 });
+
+		//add event listeners to each section
+		section.addEventListener('mouseenter', createHoverReveal);
+		section.addEventListener('mouseleave', createHoverReveal);
+	});
+}
+
+function getTextHeight(textCopy) {
+	return textCopy.clientHeight;
+}
+
+function createHoverReveal(e) {
+	console.log(e.type);
+
+	const { imageBlock, image, mask, text, textCopy, textMask, textP } =
+		e.target; //! ovo. WOW
+	// console.log(imageBlock, mask);
+	// console.log(e.target.dataset.color);
+
+	let tl = gsap.timeline({
+		defaults: {
+			duration: 0.7,
+			ease: 'Power4.out',
+		},
+	});
+
+	if (e.type === 'mouseenter') {
+		tl.to([mask, imageBlock, textMask, textP], { yPercent: 0 })
+			.to(text, { y: -getTextHeight(textCopy) / 2 }, 0)
+			.to(image, { duration: 1.1, scale: 1 }, 0)
+			.to(
+				e.target.closest('.reveal-gallery'),
+				{ backgroundColor: e.target.dataset.color },
+				0
+			);
+	} else if (e.type === 'mouseleave') {
+		tl.to([mask, textP], { yPercent: 100 })
+			.to([imageBlock, textMask], { yPercent: -101 }, 0) //! ova nula je bitna, da bi se ove dve animacije istovremeno odigrale, inace je jako cudno, pogledaj.
+			.to(text, { y: 0 }, 0)
+			.to(image, { scale: 1.2 }, 0);
+	}
+
+	return tl;
+}
+
 function moveImages(e) {
 	//! bitan css pointer-events: none; tipa na logo, na circle, da ne blokiramo ovaj mousemove kad "hoverujemo" preko ovih elemenata
 	const { offsetX, offsetY, target } = e;
@@ -89,8 +153,61 @@ function moveImages(e) {
 function init() {
 	initNavigation();
 	initHeaderTilt();
+	// initHoverReveal();
 }
 
 window.addEventListener('load', function () {
 	init();
 });
+
+//define a breakpoint
+const mq = window.matchMedia('(min-width: 768px)'); // kopiramo css media-query fazon
+
+// add change listener to this breakpoint
+mq.addListener(handleWidthChange);
+// mq.addEventListener('change', handleWidthChange);
+
+// first page load
+handleWidthChange(mq);
+
+// reset all props
+function resetProps(elements) {
+	console.log(elements);
+
+	// 	stop all tweens
+	gsap.killTweensOf('*');
+
+	if (elements.length) {
+		elements.forEach(el => {
+			el && gsap.set(el, { clearProps: 'all' }); //? el && prvo se pitamo da li je el undefined, ako nije clearujemo props, tj brisemo inline style
+		});
+	}
+}
+
+// mediaquery change
+function handleWidthChange(mq) {
+	console.log(mq);
+
+	// check if we are on the right breakpoint
+	if (mq.matches) {
+		// setup hover animation
+		initHoverReveal();
+		return;
+	}
+	// width is less then 768px
+	console.log('we are on mobile');
+
+	// remove even listeners from all sections
+	sections.forEach(section => {
+		console.log(section.imageBlock);
+		section.removeEventListener('mouseenter', createHoverReveal);
+		section.removeEventListener('mouseleave', createHoverReveal);
+
+		const { imageBlock, image, mask, text, textCopy, textMask, textP } =
+			section;
+
+		console.log(image); // undefined, i svi su undefined, msm wtf
+
+		resetProps([imageBlock, image, mask, text, textCopy, textMask, textP]);
+	});
+}
